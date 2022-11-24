abf4272ef9b6d87add1954188cca142e2947ab57
HoaAyWK <vycoi1511@gmail.com>
2022-11-24 17:26:36 +0700

    remove old fe

--- frontend/src/api/authApi.js
+++
@@ -1,8 +0,0 @@
-import axiosClient from "./axiosClient";
-const authApi = {
-  login(data) {
-    const url = "/api/v1/Auth/login";
-    return axiosClient.post(url, data);
-  },
-};
-export default authApi;
--- frontend/src/constant/status.js
+++
@@ -1,6 +0,0 @@
-export const action_status = {
-  IDLE: "idle",
-  LOADING: "loading",
-  SUCCEEDED: "succeeded",
-  FAILED: "failed",
-};
